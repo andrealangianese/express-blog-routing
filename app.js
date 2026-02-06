340c1538a19e191de94d9b67ef60896a11c79e2f
andrealangianese <andre.langia.01@gmail.com>
2026-02-06 14:56:33 +0100

    inserisco CRUD e testo su postman

--- app.js
+++ app.js
@@ -14,6 +14,40 @@ app.get("/", (req, res) => {
     res.send("dubug")
 })
 
+//creo  rotte crud
+
+// index
+app.get('/posts', function (req, res) {
+    res.send('Lista dei post');
+});
+
+// show
+app.get('/posts/:id', function (req, res) {
+    res.send('Dettagli dei posts ' + req.params.id);
+});
+
+// store
+app.post('/posts', function (req, res) {
+    res.send('Creazione nuovi posts');
+});
+
+// update
+app.put('/posts/:id', function (req, res) {
+    res.send('Modifica integrale dei posts ' + req.params.id);
+});
+
+// modify
+app.patch('/posts/:id', function (req, res) {
+    res.send('Modifica parziale dei posts ' + req.params.id);
+});
+
+// destroy
+app.delete('/posts/:id', (req, res) => {
+    res.send('Eliminazione dei posts ' + req.params.id);
+});
+
+
+
 
 app.listen(port, () => {
     console.log(`sono la porta in ascolto numero ${port}`);
